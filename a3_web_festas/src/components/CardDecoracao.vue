2f969eec0a7e983711bf9376769a758a54cacf3b
Enzo <davila.commercial@gmail.com>
2025-06-16 20:23:26 -0300

    corrigir produto para pinia

--- a3_web_festas/src/components/CardDecoracao.vue
+++ a3_web_festas/src/components/CardDecoracao.vue
@@ -1,61 +1,74 @@
 <template>
-  <q-card class="Card">
-    <q-card-section>
-      <div class="img" :style="{ backgroundImage: 'url(' + produto.img + ')' }"></div>
-      <q-card-title>{{ produto.nome }}</q-card-title>
-      <q-card-subtitle>R$ {{ produto.preco.toFixed(2) }}</q-card-subtitle>
-    </q-card-section>
-    <q-card-section>
-      <div class="q-pa-md">
-    <div class="q-mb-sm">
-    </div>
+  <div>
+    <q-card
+      class="Card"
+      v-for="produto in carrinhoStore.produtos"
+      :key="produto.id"
+    >
+      <q-card-section>
+        <div
+          class="img"
+          :style="{ backgroundImage: 'url(' + produto.img + ')' }"
+        ></div>
+        <q-card-title>{{ produto.nome }}</q-card-title>
+        <q-card-subtitle>R$ {{ produto.preco.toFixed(2) }}</q-card-subtitle>
+      </q-card-section>
+      <q-card-section>
+        <div class="q-pa-md">
+          <div class="q-mb-sm"></div>
 
-    <q-btn label="Alugar" color="primary">
-      <q-popup-proxy @before-show="updateProxy" cover transition-show="scale" transition-hide="scale">
-        <q-date v-model="proxyDate">
-          <div class="row items-center justify-end q-gutter-sm">
-            <q-btn label="Cancelar" color="primary" flat v-close-popup />
-            <q-btn label="Adicionar ao Carrinho" color="primary" flat @click="adicionarAoCarrinho" v-close-popup />
-          </div>
-        </q-date>
-      </q-popup-proxy>
-    </q-btn>
+          <q-btn label="Alugar" color="primary">
+            <q-popup-proxy
+              @before-show="updateProxy"
+              cover
+              transition-show="scale"
+              transition-hide="scale"
+            >
+              <q-date v-model="proxyDate">
+                <div class="row items-center justify-end q-gutter-sm">
+                  <q-btn label="Cancelar" color="primary" flat v-close-popup />
+                  <q-btn
+                    label="Adicionar ao Carrinho"
+                    color="primary"
+                    flat
+                    @click="adicionarAoCarrinho(produto.id)"
+                    v-close-popup
+                  />
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-btn>
+        </div>
+      </q-card-section>
+    </q-card>
   </div>
-    </q-card-section>
-  </q-card>  
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
+import { useCarrinhoStore } from "src/stores/carrinho-store";
+
 export default {
-  props: {
-    produto: {
-      nome: String,
-      preco: Number,
-      img: String
-    },
-  },
-  methods: {
-    adicionarAoCarrinho() {
-     //FAZER FUNCIONAR ADICIONAR AO CARRINHO
-    },
-  },
-  setup () {
-    const date = ref('2025/06/03')
-    const proxyDate = ref('2025/12/31')
+  setup() {
+    const carrinhoStore = useCarrinhoStore();
+    const date = ref("2025/06/03");
+    const proxyDate = ref("2025/12/31");
+
+    const updateProxy = () => {
+      proxyDate.value = date.value;
+    };
+
+    const adicionarAoCarrinho = (produtoId) => {
+      carrinhoStore.adicionarAoCarrinho(produtoId, 1);
+    };
 
     return {
+      carrinhoStore,
       date,
       proxyDate,
-
-      updateProxy () {
-        proxyDate.value = date.value
-      },
-
-      save () {
-        date.value = proxyDate.value
-      },
-    }
+      updateProxy,
+      adicionarAoCarrinho,
+    };
   },
 };
 </script>
