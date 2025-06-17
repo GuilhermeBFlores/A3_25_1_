385657240739efe3a52333233afadf10dc626638
GuilhermeBFlores <guilhermeflores2308@gmail.com>
2025-06-16 21:30:25 -0300

    Os produtos estão interagindo com o carrinho

--- a3_web_festas/src/pages/DecoracoesPage.vue
+++ a3_web_festas/src/pages/DecoracoesPage.vue
@@ -1,8 +1,21 @@
-<template> 
-<q-page class="flex flex-center">
-    <CardDecoracao :produto="{ nome: 'Bem 10 ', preco: 79.90, img: 'https://impaktovisual.com.br/12266-large_default/kit-festa-os-backyardigans-ouro.jpg' }"/>
-  </q-page>
+<template>
+  <q-page class="q-pa-md">
+    <q-toolbar class="q-mb-md">
+      <q-toolbar-title>Aluguel de Decorações</q-toolbar-title>
+    </q-toolbar>
 
+    <q-card-section>
+      <div class="row q-col-gutter-md q-row-gutter-md">
+        <div
+          v-for="(produto, index) in produtos"
+          :key="index"
+          class="col-xs-12 col-sm-6 col-md-4"
+        >
+          <CardDecoracao :produto="produto" />
+        </div>
+      </div>
+    </q-card-section>
+  </q-page>
 </template>
 
 <script>
@@ -12,6 +25,42 @@ export default {
   name: 'DecoracoesPage',
   components: {
     CardDecoracao
+  },
+  data() {
+    return {
+      produtos: [
+        {
+          nome: 'Ben 10',
+          preco: 79.90,
+          img: 'https://impaktovisual.com.br/12266-large_default/kit-festa-os-backyardigans-ouro.jpg'
+        },
+        {
+          nome: 'Backyardigans',
+          preco: 69.90,
+          img: 'https://www.artealegria.com.br/wp-content/uploads/2024/06/ben10.jpeg'
+        },
+        {
+          nome: 'Galinha Pintadinha',
+          preco: 85.00,
+          img: 'https://impaktovisual.com.br/12266-large_default/kit-festa-os-backyardigans-ouro.jpg'
+        },
+        {
+          nome: 'Backyardigans',
+          preco: 74.50,
+          img: 'https://www.artealegria.com.br/wp-content/uploads/2024/06/ben10.jpeg'
+        },
+        {
+          nome: 'Patrulha Canina',
+          preco: 90.00,
+          img: 'https://impaktovisual.com.br/12266-large_default/kit-festa-os-backyardigans-ouro.jpg'
+        },
+        {
+          nome: 'Frozen',
+          preco: 92.00,
+          img: 'https://www.artealegria.com.br/wp-content/uploads/2024/06/ben10.jpeg'
+        }
+      ]
+    }
   }
 }
 </script>
